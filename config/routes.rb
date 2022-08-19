b7500bca38377d069aa13055a14e432e98f01b20
Emre Ebeturk <emreebeturk@gmail.com>
2022-08-19 16:53:03 +0200

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
   Rails.application.routes.draw do
-    get 'bookmarks/new'
-    get 'bookmarks/create'
-    get 'bookmarks/destroy'
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     root to: "lists#index"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
     resources :lists do
       resources :bookmarks, only: [:new, :create]
     end
